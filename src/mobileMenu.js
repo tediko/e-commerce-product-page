a7ec172ed470c0655d4edca081b85aa7db0867a9
tediko <tediko123@wp.pl>
2024-04-10 15:46:30 +0200

    Added html element and disabled overflow when menu is active

--- src/mobileMenu.js
+++ src/mobileMenu.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector('[data-menu]');
 const menuList = document.querySelector('[data-menu-list]');
 const overlay = document.querySelector('[data-overlay]');
+const htmlElement = document.querySelector('html');
 let isMenuOpen = false;
 let isAnimationEnd = true;
 let openClass = 'open';
@@ -17,22 +18,24 @@ const toggleMenu = () => {
     if (isMenuOpen) {
         overlay.classList.add(openClass);
         menuList.classList.add(openClass);
-
+        htmlElement.style.overflowY = 'hidden';
+        
         menuList.addEventListener('animationend', function navOpen() {
             isAnimationEnd = !isAnimationEnd;
             menuList.removeEventListener('animationend', navOpen);
         })
-
+        
         overlay.addEventListener('click', toggleMenu);
     } else {
         overlay.classList.add(closeClass);
         menuList.classList.add(closeClass);
-
+        
         menuList.addEventListener('animationend', function navClose() {
             isAnimationEnd = !isAnimationEnd;
             overlay.classList.remove(openClass, closeClass);
             menuList.classList.remove(openClass, closeClass);
-
+            htmlElement.style.overflowY = 'unset';
+            
             menuList.removeEventListener('animationend', navClose);
             overlay.removeEventListener('click', toggleMenu);
         })
