e2dc636e2dbb2412cd1bef0b23f0676943730b0f
tediko <tediko123@wp.pl>
2024-04-11 12:25:41 +0200

    Added slider selector and glide.on event handler

--- src/slider.js
+++ src/slider.js
@@ -1,10 +1,17 @@
 import Glide from '@glidejs/glide'
+const SLIDER_SELECTOR = document.querySelector('[data-glide]');
 
-export default new Glide('.glide', {
-    type: 'carousel',
-    startAt: 0,
-    perView: 1,
-    gap: 10,
-    keyboard: true,
-    animationDuration: 600,
-  }).mount();
+let glide = new Glide(SLIDER_SELECTOR, {
+  type: 'carousel',
+  startAt: 2,
+  perView: 1,
+  gap: 10,
+  keyboard: true,
+  animationDuration: 600,
+})
+
+glide.on(['mount.before', 'run'], function() {
+  SLIDER_SELECTOR.dataset.glideIdx = glide._i;
+})
+
+export default glide.mount();
