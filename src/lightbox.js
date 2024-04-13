0211ac2debfed361745a95c65f7d3b446b5e2d67
tediko <tediko123@wp.pl>
2024-04-13 17:28:39 +0200

    Added focus trap

--- src/lightbox.js
+++ src/lightbox.js
@@ -5,10 +5,39 @@ const CLOSE_BUTTON_SELECTOR = document.querySelector('[data-lightbox-close]');
 const MAIN_SLIDER_SELECTOR = document.querySelector('[data-glide]');
 const MAIN_SLIDER_TRACK_SELECTOR = document.querySelector('[data-main-slider-track]');
 const OVERLAY_SELECTOR = document.querySelector('[data-overlay]');
+const SHOW_BUTTON_SELECTOR = document.querySelector('[data-lightbox-show]');
 
 let mediaQuery = '(min-width: 769px)';
 let mediaQueryList = window.matchMedia(mediaQuery);
 let isInitialized = false;
+let focusedElementBeforeCartOpen;
+
+const focusTrap = () => {
+    let focusableElements = LIGHTBOX_CONTAINER_SELECTOR.querySelectorAll('a[href]:not([disabled]), button:not([disabled])');
+    let firstFocusableEl = focusableElements[0];
+    let lastFocusableEl = focusableElements[focusableElements.length - 1];
+    let KEYCODE_TAB = 9;
+
+    focusedElementBeforeCartOpen = document.activeElement;
+    firstFocusableEl.focus();
+
+    LIGHTBOX_CONTAINER_SELECTOR.addEventListener('keydown', (e) => {
+        let isTabPressed = (e.key === 'tab' || e.keyCode === KEYCODE_TAB);
+        if (!isTabPressed) return;
+
+        if (e.shiftKey) {
+            if (document.activeElement === firstFocusableEl) {
+                lastFocusableEl.focus();
+                e.preventDefault();
+            }
+        } else {
+            if (document.activeElement === lastFocusableEl) {
+                firstFocusableEl.focus();
+                e.preventDefault();
+            }
+        }
+    })
+}
 
 // Initialize a Glide
 let glide = new Glide(LIGHTBOX_SLIDER_SELECTOR, {
@@ -40,6 +69,7 @@ const showLightbox = () => {
     OVERLAY_SELECTOR.classList.add('open');    
     isInitialized ? null : glide.mount();
     isInitialized = true;
+    focusTrap()
 }
 
 // Hides lightbox visually and removes event listener
@@ -52,6 +82,7 @@ const removeLightbox = () => {
 const hideLightbox = () => {
     LIGHTBOX_CONTAINER_SELECTOR.style.display = "none";
     OVERLAY_SELECTOR.classList.remove('open', 'close');
+    focusedElementBeforeCartOpen.focus();
 }
 
 // Adds event listener on initialization if mediaQuery matches
@@ -72,5 +103,6 @@ mediaQueryList.addEventListener('change', event => {
 })
 CLOSE_BUTTON_SELECTOR.addEventListener('click', hideLightbox);
 OVERLAY_SELECTOR.addEventListener('click', hideLightbox);
+SHOW_BUTTON_SELECTOR.addEventListener('click', showLightbox);
 
 export default glide;
